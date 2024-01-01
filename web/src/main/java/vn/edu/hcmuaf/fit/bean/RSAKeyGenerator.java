409c145650a64955885fcacc994e3d05c8f57de9
khacnhuan14102002 <20130090@st.hcmuaf.edu.vn>
2024-01-01 21:18:28 +0700

    fix key

--- web/src/main/java/vn/edu/hcmuaf/fit/bean/RSAKeyGenerator.java
+++ web/src/main/java/vn/edu/hcmuaf/fit/bean/RSAKeyGenerator.java
@@ -10,23 +10,50 @@ public class RSAKeyGenerator {
 
     public RSAKeyGenerator() throws NoSuchAlgorithmException {
         this.keyGen = KeyPairGenerator.getInstance("RSA");
-        this.keyGen.initialize(2048);
+        this.keyGen.initialize(1024);
     }
 
-    public void createKeys() {
-        this.pair = this.keyGen.generateKeyPair();
+
+    public KeyPair createKey() {
+        try {
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPairGenerator.initialize(1024);
+            return keyPairGenerator.generateKeyPair();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    public static String convertPublicKeyToString(PublicKey publicKey) {
+        byte[] publicKeyBytes = publicKey.getEncoded();
+        return Base64.getEncoder().encodeToString(publicKeyBytes);
+    }
+    public static String convertPrivateKeyToString(PrivateKey privateKey) {
+        byte[] publicKeyBytes =privateKey.getEncoded();
+        return Base64.getEncoder().encodeToString(publicKeyBytes);
+    }
+    public String signData(String data, PrivateKey privateKey) {
+        try {
+            Signature signature = Signature.getInstance("SHA256withRSA");
+            signature.initSign(privateKey);
+            signature.update(data.getBytes());
+            byte[] signatureBytes = signature.sign();
+            return Base64.getEncoder().encodeToString(signatureBytes);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
-    public String getPublicKey() {
-        PublicKey publicKey = pair.getPublic();
-        byte[] publicBytes = publicKey.getEncoded();
-        return Base64.getEncoder().encodeToString(publicBytes);
+    public boolean verifySignature(String data, String signature, PublicKey publicKey) {
+        try {
+            Signature sig = Signature.getInstance("SHA256withRSA");
+            sig.initVerify(publicKey);
+            sig.update(data.getBytes());
+            return sig.verify(Base64.getDecoder().decode(signature));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
-
-    public String getPrivateKey() {
-        PrivateKey privateKey = pair.getPrivate();
-        byte[] privateBytes = privateKey.getEncoded();
-        return Base64.getEncoder().encodeToString(privateBytes);
-    }
-
 }
--- web/src/main/java/vn/edu/hcmuaf/fit/service/KeyService.java
+++ web/src/main/java/vn/edu/hcmuaf/fit/service/KeyService.java
@@ -1,6 +1,7 @@
 package vn.edu.hcmuaf.fit.service;
 
 import java.security.KeyPair;
+import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.sql.Connection;
@@ -9,6 +10,8 @@ import java.sql.ResultSet;
 import java.sql.Timestamp;
 import java.util.Date;
 
+import com.google.api.client.util.DateTime;
+import org.apache.poi.util.SystemOutLogger;
 import vn.edu.hcmuaf.fit.bean.*;
 import vn.edu.hcmuaf.fit.bean.category;
 import vn.edu.hcmuaf.fit.db.connect;
@@ -18,29 +21,49 @@ public class KeyService {
     static PreparedStatement ps = null;
     static ResultSet rs = null;
 
-    public static void addKey(int idu, String pub, String pri) {
-        String query = "INSERT INTO  userkeys(UserId, PublicKey, PrivateKey)\n" +
-                "VALUES (?,?,?);";
+    public static void addKey(Key key) {
+        String query = "INSERT INTO  userkeys(UserId, PublicKey, ReportDate, DayCreate,isblock)\n" +
+                "VALUES (?,?,?,?,?);";
         try {
             conn = new connect().getconConnection();
             ps = conn.prepareStatement(query);
-            ps.setInt(1, idu);
-            ps.setString(2, pub);
-            ps.setString(3, pri);
+            ps.setInt(1, key.getIdU());
+            ps.setString(2, key.getPubkey());
+            ps.setTimestamp(3,key.getReportdate());
+            ps.setTimestamp(4,key.getAddedDate());
+            ps.setInt(5, 0);
+
             ps.executeUpdate();
         } catch (Exception e) {
             System.out.println(e.getMessage());
             System.out.println("fail");
         }
     }
+    public static String getPublic(int id) {
+        String query = "select PublicKey from userkeys where UserId= ?";
+        try {
+            conn = new connect().getconConnection();
+            ps = conn.prepareStatement(query);
+            ps.setInt(1,id);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                String publicKey = rs.getString(1);
+                System.out.println("Public Key from Database: " + publicKey);
+                return publicKey;
 
+            }
+        } catch (Exception e) {
+        }
+        return null;
+    }
     public static void reportKey(String pubkey, Timestamp time) {
-        String query = "UPDATE userkeys set ReportDate= ? where PublicKey=?";
+        String query = "UPDATE userkeys set ReportDate= ?,isblock=1  where PublicKey=?";
         try {
             conn = new connect().getconConnection();
             ps = conn.prepareStatement(query);
             ps.setTimestamp(1, time);
             ps.setString(2, pubkey);
+
             ps.executeUpdate();
         } catch (Exception e) {
             System.out.println(e.getMessage());
@@ -49,28 +72,29 @@ public class KeyService {
     }
 
 
-    public static void main(String[] args) {
-        Date date = new Date();
-        Timestamp time = new Timestamp(date.getTime());
-        KeyService kyes = new KeyService();
-        kyes.reportKey("asdaxacscascaczx", time);
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+//        Date date = new Date();
+//        Timestamp time = new Timestamp(date.getTime());
+//        KeyService kyes = new KeyService();
+//        kyes.reportKey("asdaxacscascaczx", time);
         KeyService keyService = new KeyService();
 
-        // Tạo key pair
-        KeyPair keyPair = new Key().createKey();
-        PublicKey publicKey = keyPair.getPublic();
-        PrivateKey privateKey = keyPair.getPrivate();
 
-        // Convert key pair to strings
-        String publicKeyString = Key.convertPublicKeyToString(publicKey);
-        String privateKeyString = Key.convertPrivateKeyToString(privateKey);
+//        KeyPair keyPair = new RSAKeyGenerator().createKey();
+//        PublicKey publicKey = keyPair.getPublic();
+//        PrivateKey privateKey = keyPair.getPrivate();
+//
+//        // Convert key pair to strings
+//        String publicKeyString = RSAKeyGenerator.convertPublicKeyToString(publicKey);
+//        String privateKeyString = RSAKeyGenerator.convertPrivateKeyToString(privateKey);
+//
+//      Key keys = new Key(3,publicKeyString,null,time);
+//        addKey(keys);
 
-        // Thêm key vào cơ sở dữ liệu
-        addKey(29, publicKeyString, privateKeyString);
-
-        // In thông tin key
-        System.out.println("Public Key: " + publicKeyString);
-        System.out.println("Private Key: " + privateKeyString);
+//        // In thông tin key
+//        System.out.println("Public Key: " + publicKeyString);
+//        System.out.println("Private Key: " + privateKeyString);
+        System.out.println(getPublic(3));
     }
 
 }
